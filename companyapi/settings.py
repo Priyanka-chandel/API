0fd6463c71bdb1c8311cf43c1a4ea888d69821fa
Priyanka chandel <priyankachandel1937@gmail.com>
2023-12-27 12:24:01 +0530

    first commit

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#bj%bjgesj%9bua07!vmx4@dgc_p3)0s0g(60_sp7wu8zv!2v&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -77,8 +78,14 @@ WSGI_APPLICATION = 'companyapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'CFggFG4fGG5edC6D*22*66c6f6EG3A1c',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'58405'
+
+        
     }
 }
 
@@ -118,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =os.path.join (BASE_DIR,'static'),
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
